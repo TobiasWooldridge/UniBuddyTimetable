37a992193f2dc66fc8d5978526ad42ab20d21b36
Russell <rdpeake@adam.com.au>
2015-02-10 16:57:03 +1030

    added notifications for unsupported browsers and better handling for partial supported browsers

--- src/app/app.js
+++ src/app/app.js
@@ -1,25 +1,27 @@
-angular.module('unibuddyTimetable', [
-        'templates-app',
-        'templates-common',
-        'unibuddyTimetable.timetable',
-        'ui.state',
-        'ui.route'
-    ])
+if (document.addEventListener !== undefined) {
+    angular.module('unibuddyTimetable', [
+            'templates-app',
+            'templates-common',
+            'unibuddyTimetable.timetable',
+            'ui.state',
+            'ui.route'
+        ])
 
-    .config(function myAppConfig($stateProvider, $urlRouterProvider, $locationProvider) {
-        $urlRouterProvider.otherwise('/');
-        $locationProvider.html5Mode(true);
-    })
+        .config(function myAppConfig($stateProvider, $urlRouterProvider, $locationProvider) {
+            $urlRouterProvider.otherwise('/');
+            $locationProvider.html5Mode(true);
+        })
 
-    .run(function run() {
-    })
+        .run(function run() {
+        })
 
-    .controller('AppCtrl', function AppCtrl($scope, $location) {
-        $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
-            if (angular.isDefined(toState.data.pageTitle)) {
-                $scope.pageTitle = toState.data.pageTitle;// + ' | Flinders Timetable';
-            }
-        });
-    })
-;
+        .controller('AppCtrl', function AppCtrl($scope, $location) {
+            $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+                if (angular.isDefined(toState.data.pageTitle)) {
+                    $scope.pageTitle = toState.data.pageTitle;// + ' | Flinders Timetable';
+                }
+            });
+        })
+    ;
 
+}
