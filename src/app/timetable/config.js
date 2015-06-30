03ef3991027848ff686dd2cd66320cd2e8597b43
Tobias Wooldridge <tobias@wooldridge.id.au>
2015-06-30 00:49:58 -0700

    Updates default semesters

--- src/app/timetable/config.js
+++ src/app/timetable/config.js
@@ -2,7 +2,7 @@ angular.module('unibuddyTimetable.config', [])
     .constant('times', {
         years: [ 2011, 2012, 2013, 2014, 2015 ],
         defaultYear: 2015,
-        defaultSemester: "S1,NS1,SP1,SP2,SP3"
+        defaultSemester: "S2,NS2,SP4,SP5,SP6"
     })
 
     .constant('timetablesPerPage', 5)
